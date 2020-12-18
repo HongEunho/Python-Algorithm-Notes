d0d968745e70ada034a7db96cdf26caa24a1f265
HongEunho <ghdwns315@naver.com>
2020-12-18 20:35:22 +0900

    Create AllSearch.py

---
+++ Implementation/AllSearch.py
@@ -0,0 +1,10 @@
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)

01264ca2caafa0425b9df29dff6d0e7140b384b0
HongEunho <ghdwns315@naver.com>
2020-12-31 23:07:44 +0900

    Update DFSvsBFS.py

--- DFS/DFSvsBFS.py
+++ DFS/DFSvsBFS.py
@@ -1,5 +1,21 @@
+from collections import deque
+
 n,m,v = map(int,input().split())
-graph = []
+graph = [[0]*m for _ in range(n)]
+visited = [False]*n
 
 for i in range(m):
-    
+    a, b = map(int,input().split())
+    graph[a-1][b-1] = graph[b-1][a-1] = 1
+
+def DFS(v):
+    visited[v] = True
+    print(v+1, end=' ')
+
+    for i in range(n):
+        if graph[v][i]==1 and not visited[i]:
+            DFS(i)
+
+
+
+DFS(v-1)

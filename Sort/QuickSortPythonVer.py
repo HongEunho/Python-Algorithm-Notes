2b5a728376f8d4736f603a4ce1fe20a5deaa91c3
HongEunho <ghdwns315@naver.com>
2021-01-16 22:35:52 +0900

    Create QuickSortPythonVer.py

---
+++ Sort/QuickSortPythonVer.py
@@ -0,0 +1,16 @@
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    # 리스트가 하나의 원소만을 가지고 있으면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))

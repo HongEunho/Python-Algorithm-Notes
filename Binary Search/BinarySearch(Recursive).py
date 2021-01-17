9cc35e7f0113e47f8891da46d43047ed5a8f0bf9
HongEunho <ghdwns315@naver.com>
2021-01-17 21:40:00 +0900

    Create BinarySearch(Recursive).py

---
+++ Binary Search/BinarySearch(Recursive).py
@@ -0,0 +1,21 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않습니다")
+else:
+    print(result + 1)

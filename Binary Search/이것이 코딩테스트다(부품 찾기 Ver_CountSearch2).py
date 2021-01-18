8231ca583cd83a3432d1980b7185eed8da25132f
HongEunho <ghdwns315@naver.com>
2021-01-18 16:41:01 +0900

    Create 이것이 코딩테스트다(부품 찾기 Ver_CountSearch2).py

---
+++ Binary Search/이것이 코딩테스트다(부품 찾기 Ver_CountSearch2).py
@@ -0,0 +1,14 @@
+n = int(input())
+array = [0]*1000001
+
+for i in input().split():
+    array[int(i)] = 1
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')

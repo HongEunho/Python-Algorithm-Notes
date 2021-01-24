a39c1371aaf182a57e6f2d1375e3d739a31b9bed
HongEunho <ghdwns315@naver.com>
2021-01-24 21:54:05 +0900

    Update BaekJoon3020.py

--- Binary Search/BaekJoon3020.py
+++ Binary Search/BaekJoon3020.py
@@ -28,8 +28,8 @@ def binary_search(array, target, start, end):
 
 
 for i in range(1, h + 1):
-    down_count = len(down) - binary_search(down, h - 0.5, 0, len(down) - 1)
-    top_count = len(up) - binary_search(up, h + 0.5, 0, len(up) - 1)
+    down_count = len(down) - binary_search(down, i - 0.5, 0, len(down) - 1)
+    top_count = len(up) - binary_search(up, h - i + 0.5, 0, len(up) - 1)
 
     if min_count == down_count + top_count:
         range_count += 1

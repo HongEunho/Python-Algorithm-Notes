26a821b6989c311ad05b0b6aee7941261e28f551
HongEunho <ghdwns315@naver.com>
2021-01-29 23:17:43 +0900

    Update test.py

--- DFS/test.py
+++ DFS/test.py
@@ -1,6 +1,12 @@
-from collections import deque
+trace = []
+trace.append((2,3))
+trace.append((3,4))
+trace.append((5,2))
+trace.append((1,6))
+result = (min(trace, key = lambda x: x[0]))[1]
+print(result)
 
-queue = deque()
-queue.append(2)
-a = queue.popleft()
-print(a)
+new_graph = [1,2,'X']
+tmp = [i for i, value in enumerate(new_graph) if value == 'X']
+
+print(min(tmp))

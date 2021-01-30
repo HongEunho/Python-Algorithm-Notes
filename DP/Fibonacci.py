eceaa43901fc221ea10a223aa7307e7c6ce675cf
HongEunho <ghdwns315@naver.com>
2021-01-31 07:48:42 +0900

    Update Fibonacci.py

--- DP/Fibonacci.py
+++ DP/Fibonacci.py
@@ -1,6 +1,14 @@
+d = [0] * 100
+
+
 def fibo(x):
     if x == 1 or x == 2:
         return 1
-    return fibo(x - 1) + fibo(x - 2)
+    if d[x] != 0:
+        return d[x]
 
-print(fibo(4))
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+
+print(fibo(99))

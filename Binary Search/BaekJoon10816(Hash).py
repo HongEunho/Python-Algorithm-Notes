2c10d33efa30fb89d597f5e720520f9a16b610c7
HongEunho <ghdwns315@naver.com>
2021-04-02 02:06:41 +0900

    Create BaekJoon10816(Hash).py

---
+++ Binary Search/BaekJoon10816(Hash).py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+n = stdin.readline().rstrip()
+card = list(map(int,stdin.readline().split()))
+m = stdin.readline().rstrip()
+test = list(map(int,stdin.readline().split()))
+
+hash = {}
+
+for i in card:
+    if i in hash:
+        hash[i] += 1
+    else:
+        hash[i] = 1
+
+for i in test:
+    if i in hash:
+        print(hash[i], end=' ')
+    else:
+        print(0, end=' ')

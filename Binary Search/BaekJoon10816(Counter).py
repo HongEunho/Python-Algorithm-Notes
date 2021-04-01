e83d62bd38371a8e34057ec34775b5bc39c0c57e
HongEunho <ghdwns315@naver.com>
2021-04-02 02:06:38 +0900

    Create BaekJoon10816(Counter).py

---
+++ Binary Search/BaekJoon10816(Counter).py
@@ -0,0 +1,15 @@
+from collections import Counter
+from sys import stdin
+
+n = stdin.readline().rstrip()
+card = list(map(int,stdin.readline().split()))
+m = stdin.readline().rstrip()
+test = list(map(int,stdin.readline().split()))
+
+count = Counter(card)
+
+for i in range(len(test)):
+    if test[i] in count:
+        print(count[test[i]], end=' ')
+    else:
+        print(0, end=' ')

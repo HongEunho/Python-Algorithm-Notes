af3453a73d69b6d16248efd85522261a62ba8ac4
HongEunho <ghdwns315@naver.com>
2021-04-05 02:49:36 +0900

    Create BaekJoon1987(BFSVer.).py

---
+++ BFS/BaekJoon1987(BFSVer.).py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+sys.setrecursionlimit(100000)
+
+r, c = map(int, input().split())
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+graph = []
+count = 1
+
+
+def bfs(x, y, z):
+    queue = deque()
+    queue.append((x, y, z))
+
+    while queue:
+        a, b, alpha = queue.popleft()
+
+        for i in range(4):
+            nx = a + x[i]
+            ny = b + y[i]
+
+            if nx < 0 or nx >= r or ny < 0 or ny >= c:
+                continue
+            if graph[nx][ny] in alpha:
+                continue
+
+            queue.append((nx, ny, alpha+graph[nx][ny]))
+            global count
+            count = max(count, len(alpha)+1)

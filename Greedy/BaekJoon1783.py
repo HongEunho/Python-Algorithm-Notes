c351c767722c3c97d12eaa784b57fde1f110b7ca
HongEunho <ghdwns315@naver.com>
2021-04-09 06:25:08 +0900

    Update BaekJoon1783.py

--- Greedy/BaekJoon1783.py
+++ Greedy/BaekJoon1783.py
@@ -1,9 +1,12 @@
-n, m = map(int, input().split())
-if n == 1:
-    print(1)
-elif n == 2:
-    print(min(4, (m-1)//2+1))
-elif m <= 6:
-    print(min(4, m))
-else:
-    print(m-2)
+i = 0
+while True:
+    i+=1
+    l, p, v = map(int, input().split())
+    if l==0 and p==0 and v==0:
+        break
+    a = v//p
+    b = v%p
+    if l<b:
+        b = l
+    print("Case %d: %d" %(i, a*l+b))
+    

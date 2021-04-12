9d25b9fb51a9e6be8c795973582b262cbb459e5b
HongEunho <ghdwns315@naver.com>
2021-04-13 05:28:12 +0900

    Create BaekJoon17298.py

---
+++ Stack/BaekJoon17298.py
@@ -0,0 +1,22 @@
+import sys
+n = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+stack = []
+for i in range(len(A)-1, 0, -1):
+    stack.append(A[i])
+
+for i in range(n-1):
+    
+
+
+
+for i in range(n-1):
+    flag = 0
+    for j in range(i+1, n):
+        if A[i] < A[j]:
+            flag = 1
+            print(A[j], end=' ')
+            break
+    if flag == 0:
+        print("-1", end=' ')
+print("-1")

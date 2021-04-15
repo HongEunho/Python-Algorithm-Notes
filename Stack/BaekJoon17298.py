a5d054669c7c258807ac192e98b17953fe70d83a
HongEunho <ghdwns315@naver.com>
2021-04-15 21:59:00 +0900

    Update BaekJoon17298.py

--- Stack/BaekJoon17298.py
+++ Stack/BaekJoon17298.py
@@ -1,22 +1,17 @@
 import sys
 n = int(input())
 A = list(map(int, sys.stdin.readline().split()))
+answer = [-1] * n
 stack = []
-for i in range(len(A)-1, 0, -1):
-    stack.append(A[i])
-
-for i in range(n-1):
 
 
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
 
 
-for i in range(n-1):
-    flag = 0
-    for j in range(i+1, n):
-        if A[i] < A[j]:
-            flag = 1
-            print(A[j], end=' ')
-            break
-    if flag == 0:
-        print("-1", end=' ')
-print("-1")
+print(*answer)
+
+

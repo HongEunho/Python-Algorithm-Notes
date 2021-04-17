24aeb316f33ebdb182ec2ddf453aeddd80cbf883
HongEunho <ghdwns315@naver.com>
2021-04-17 21:58:37 +0900

    Create BaekJoon5430.py

---
+++ Queue/BaekJoon5430.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+t = int(input())
+
+for i in range(t):
+    p = sys.stdin.readline().rstrip()
+    n = int(input())
+    arr = sys.stdin.readline().rstrip()[1:-1].split(",")
+    queue = deque(arr)
+
+    rev, front, back = 0, 0, len(queue)-1
+    flag = 0
+    if n == 0:
+        queue = []
+        front = 0
+        back = 0
+
+    for j in p:
+        if j == 'R':
+            rev += 1
+        elif j == 'D':
+            if len(queue) < 1:
+                flag = 1
+                print("error")
+                break
+            else:
+                if rev % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
+    if flag == 0:
+        if rev % 2 == 0:
+            print("[" + ",".join(queue) + "]")
+        else:
+            queue.reverse()
+            print("[" + ",".join(queue) + "]")
+

ca661f7f5551a408261ff151553dd4b64ff37f5e
HongEunho <ghdwns315@naver.com>
2021-04-20 02:53:59 +0900

    Update BaekJoon2156.py

--- DP/BaekJoon2156.py
+++ DP/BaekJoon2156.py
@@ -1,8 +1,20 @@
 n = int(input())
 
-grape = []
+wine = []
 
 for i in range(n):
-    grape.append(int(input()))
+    wine.append(int(input()))
 
-print(d)
+d = [0]*n
+
+d[0] = wine[0]
+if n > 1:
+    d[1] = wine[0]+wine[1]
+
+if n > 2:
+    d[2] = max(wine[2]+wine[1], wine[2]+wine[0], d[1])
+
+for i in range(3, n):
+    d[i] = max(d[i-1], d[i-3]+wine[i-1]+wine[i], d[i-2]+wine[i])
+
+print(d[n-1])

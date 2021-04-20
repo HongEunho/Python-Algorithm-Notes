0115862b13f79e93e895b8143e34dd3d089b6ca8
HongEunho <ghdwns315@naver.com>
2021-04-20 23:29:44 +0900

    Update BaekJoon2225.py

--- DP/BaekJoon2225.py
+++ DP/BaekJoon2225.py
@@ -1,7 +1,13 @@
 n, k = map(int, input().split())
 
 d = [[0]*(k+1) for _ in range(n+1)]
-for i in range(1, n):
+
+for i in range(k+1):
+    d[0][i] = 1
+
+for i in range(1, n+1):
     for j in range(1, k+1):
-        for k in range(i):
-            
+        d[i][j] = d[i][j-1] + d[i-1][j]
+        d[i][j] %= 1000000000
+
+print(d[n][k])

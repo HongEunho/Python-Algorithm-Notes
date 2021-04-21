ea4ba20e2e920f3cad4834b3d5fa90047b014f55
HongEunho <ghdwns315@naver.com>
2021-04-21 20:18:20 +0900

    Create BaekJoon12865.py

---
+++ DP/BaekJoon12865.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+thing = [[0,0]]
+d = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(n):
+    thing.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = thing[i][0]
+        v = thing[i][1]
+
+        if j < w:
+            d[i][j] = d[i-1][j]
+        else:
+            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
+
+print(d[n][k])

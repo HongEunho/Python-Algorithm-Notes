2fc6b6fedfa680f8c062efb88d50bd9212581cdc
HongEunho <ghdwns315@naver.com>
2021-04-22 01:06:31 +0900

    Create Programmers불량 사용자.py

---
+++ String/Programmers불량 사용자.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+
+
+def match(candidate, banid):
+    for i in range(len(banid)):
+        if banid[i] == "*":
+            continue
+        if banid[i] != candidate[i]:
+            return False
+
+    return True
+
+
+def check(candidates, banned_id):
+    for i in range(len(banned_id)):
+        if len(candidates[i]) != len(banned_id[i]):
+            return False
+        for a, b in zip(candidates[i], banned_id[i]):
+            if b == "*":
+                continue
+            if a != b:
+                return False
+    return True
+
+
+def solution(user_id, banned_id):
+    answer = []
+
+    for candidates in permutations(user_id, len(banned_id)):
+        if check(candidates, banned_id):
+            candidates = set(candidates)
+            if candidates not in answer:
+                answer.append(candidates)
+
+    return len(answer)
+

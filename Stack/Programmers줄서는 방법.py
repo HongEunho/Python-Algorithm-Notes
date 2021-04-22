b3dc9c892bfa8dd1e8b758b87a914efd2649c6ab
HongEunho <ghdwns315@naver.com>
2021-04-23 03:00:32 +0900

    Create Programmers줄서는 방법.py

---
+++ Stack/Programmers줄서는 방법.py
@@ -0,0 +1,23 @@
+import math
+
+
+def solution(n, k):
+    answer = [i for i in range(1, n + 1)]
+    stack = []
+    k = k-1
+
+
+    while answer:
+        # k / (n-1)! 을 했을 때의 몫이 맨 첫번째 자리
+        a = k // math.factorial(n - 1)
+        stack.append(answer[a])
+        del answer[a]
+
+        # k를 줄여나간다.
+        k = k % math.factorial(n - 1)
+        n -= 1
+
+
+    return stack
+
+print(solution(4, 6))

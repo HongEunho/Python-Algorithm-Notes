8198cef5c7c5b7d8edb3822d9edf4abfde0abe81
HongEunho <ghdwns315@naver.com>
2021-04-24 01:32:50 +0900

    Update Programmers 순위 검색(효율성실패).py

--- Binary Search/Programmers 순위 검색(효율성실패).py
+++ Binary Search/Programmers 순위 검색(효율성실패).py
@@ -9,6 +9,7 @@ def solution(info, query):
     for i in range(len(info)):
         infol.append(info[i].split())
         # tmp = ['java', 'backend', 'junior', 'pizza', '150']
+        print(infol)
     for j in range(len(query)):
         queryl.append(query[j].split())
         while len(queryl[j]) > 5:
@@ -27,3 +28,5 @@ def solution(info, query):
                     answer[j] += 1
 
     return answer
+print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],
+               ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))

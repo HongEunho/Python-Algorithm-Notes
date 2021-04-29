c98bb92cb4d410fac27e998ab3bba1bd1ac6bbd3
HongEunho <ghdwns315@naver.com>
2021-04-29 19:00:41 +0900

    Update BaekJoon15658.py

--- String/BaekJoon15658.py
+++ String/BaekJoon15658.py
@@ -2,18 +2,29 @@ n = int(input())
 num = list(map(int, input().split()))
 plus, minus, mul, div = map(int, input().split())
 
-mymax = int(1e9)
-mymin = int(-1e9)
+mymax = int(-1e9)
+mymin = int(1e9)
 
-def dfs(index, result):
+def dfs(index, result, plus, minus, mul, div):
     global mymax, mymin
     if index == n:
         mymax = max(mymax, result)
         mymin = min(mymin, result)
+        return
+    if plus > 0 :
+        dfs(index + 1, result + num[index], plus-1, minus, mul, div)
+    if minus > 0 :
+        dfs(index + 1, result - num[index], plus, minus-1, mul, div)
+    if mul > 0:
+        dfs(index + 1, result * num[index], plus, minus, mul-1, div)
+    if div > 0 :
+        dfs(index + 1, int(result / num[index]), plus, minus, mul, div-1)
 
 
 
-dfs(1, num[0])
+dfs(1, num[0], plus, minus, mul, div)
+print(mymax)
+print(mymin)
 
 
 

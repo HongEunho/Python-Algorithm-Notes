8295106139f8d516ab975b98e98b6ea330318dec
HongEunho <ghdwns315@naver.com>
2021-05-06 21:37:59 +0900

    Create Programmers 경주로 건설.py

---
+++ DFS/Programmers 경주로 건설.py
@@ -0,0 +1,46 @@
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def dfs(graph, price, x, y, fx, fy, n):
+
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if nx<0 or nx>=n or ny<0 or ny>=n:
+            continue
+        if graph[nx][ny] == 1:
+            continue
+
+
+        if fx!=nx and fy!= ny:
+            if price[x][y]+600 < price[nx][ny]:
+                a = price[nx][ny]
+                price[nx][ny] = price[x][y]+600
+                dfs(graph, price, nx, ny, x, y, n)
+                price[nx][ny] = a
+        else:
+            if price[x][y]+100 < price[nx][ny]:
+                a = price[nx][ny]
+                price[nx][ny] = price[x][y]+100
+                dfs(graph, price, nx, ny, x, y, n)
+                price[nx][ny] = a
+
+def solution(board):
+    answer = 0
+    n = len(board)
+    price = [[int(1e9)]*n for _ in range(n)]
+    price[0][0] = 0
+    dfs(board, price, 0, 0, 0, 0, n)
+    answer = price[n-1][n-1]
+
+    return answer
+
+print(solution([[0, 1, 1, 1, 1, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 1, 1, 1, 1],
+[1, 1, 1, 1, 0, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 1, 1, 1, 1],
+[0, 1, 0, 1, 1, 1, 1, 1, 1],
+[0, 1, 0, 0, 0, 1, 1, 1, 1],
+[0, 1, 1, 1, 0, 1, 1, 1, 1],
+[0, 0, 0, 0, 0, 1, 1, 1, 1],
+[1, 1, 1, 1, 0, 0, 0, 0, 0]]))

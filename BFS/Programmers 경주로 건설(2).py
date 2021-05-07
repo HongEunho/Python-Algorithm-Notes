9a121809cb9c64327c5f0ab773a72bf25f1eaeab
HongEunho <ghdwns315@naver.com>
2021-05-08 00:21:13 +0900

    Create Programmers 경주로 건설(2).py

---
+++ BFS/Programmers 경주로 건설(2).py
@@ -0,0 +1,51 @@
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(board):
+    n = len(board)
+    price = [[int(1e9)] * n for _ in range(n)]
+    price[0][0] = 0
+
+    queue = deque()
+    queue.append((0, 0, 0, 5))  # (시작X, 시작Y, 시작Cost, 시작방향)
+
+    while queue:
+        x, y, c, z = queue.popleft()
+
+        if x == n - 1 and y == n - 1:
+            continue
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = i
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if board[nx][ny] == 1:
+                continue
+
+            if z == 5:
+                nc = c + 100
+
+            elif nz == z:
+                nc = c + 100
+
+            else:
+                nc = c + 600
+
+            if nc <= price[nx][ny]:
+                price[nx][ny] = nc
+                queue.append((nx, ny, nc, i))
+
+    return price[-1][-1]
+
+
+def solution(board):
+    n = len(board)
+    answer = bfs(board)
+    return answer
+

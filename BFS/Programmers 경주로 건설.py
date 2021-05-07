f90c7a80577d81c65106a6cd191883cfac09fe4a
HongEunho <ghdwns315@naver.com>
2021-05-07 23:56:25 +0900

    Update Programmers 경주로 건설.py

--- BFS/Programmers 경주로 건설.py
+++ BFS/Programmers 경주로 건설.py
@@ -3,25 +3,17 @@ from collections import deque
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def samedir(a, b):
-    if (a==0 or a==1) and (b==2 or b==3):
-        return 600
-    elif (a==2 or a == 3) and (b==0 or b==1):
-        return 600
-    else:
-        return 100
 
-def bfs(board, sx, sy, sz, n):
+def bfs(board, dir):
+    n = len(board)
     price = [[int(1e9)] * n for _ in range(n)]
     price[0][0] = 0
 
-    result = []
-
     queue = deque()
-    queue.append((sx, sy, sz))
+    queue.append((0, 0, 0, dir))  # (시작X, 시작Y, 시작Cost, 시작방향)
 
     while queue:
-        x, y, z = queue.popleft()
+        x, y, c, z = queue.popleft()
 
         if x == n - 1 and y == n - 1:
             continue
@@ -36,29 +28,20 @@ def bfs(board, sx, sy, sz, n):
             if board[nx][ny] == 1:
                 continue
 
-            if z == 5:
-                cost = price[x][y] + 100
+            if nz == z:
+                nc = c + 100
             else:
-                cost = price[x][y] + samedir(z, nz)
+                nc = c + 600
 
-            if cost < price[nx][ny]:
-                price[nx][ny] = cost
-                queue.append((nx, ny, i))
+            if nc < price[nx][ny]:
+                price[nx][ny] = nc
+                queue.append((nx, ny, nc, i))
 
     return price[-1][-1]
 
 
 def solution(board):
     n = len(board)
-    answer = bfs(board, 0, 0, 5, n)
+    answer = min(bfs(board, 0), bfs(board, 2))
     return answer
 
-print(solution([[0, 1, 1, 1, 1, 1, 1, 1, 1],
-[0, 0, 0, 0, 0, 1, 1, 1, 1],
-[1, 1, 1, 1, 0, 1, 1, 1, 1],
-[0, 0, 0, 0, 0, 1, 1, 1, 1],
-[0, 1, 0, 1, 1, 1, 1, 1, 1],
-[0, 1, 0, 0, 0, 1, 1, 1, 1],
-[0, 1, 1, 1, 0, 1, 1, 1, 1],
-[0, 0, 0, 0, 0, 1, 1, 1, 1],
-[1, 1, 1, 1, 0, 0, 0, 0, 0]]))

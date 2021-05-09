a9a43774df20b783b1ba7977b685f4672b9c55e7
HongEunho <ghdwns315@naver.com>
2021-05-09 22:10:23 +0900

    Update test2.py

--- DFS/test2.py
+++ DFS/test2.py
@@ -1,26 +1,3 @@
-from itertools import combinations
+a = "*"
 
-
-p, n, h = map(int,input().split())
-
-pc = [[]*n for _ in range(p)]
-result = [[]*1 for _ in range(p)]
-
-for i in range(n):
-    a, b = map(int, input().split())
-    pc[a-1].append(b)
-
-maxPc = 0
-
-for i in range(p):
-    for j in range(len(pc[i])):
-        tmp = list(combinations(pc[i], j))
-        for k in range(len(tmp)):
-            if maxPc <= sum(tmp[k]) <= h:
-                maxPc = sum(tmp[k])
-
-    result[i] = maxPc
-
-for i in range(p):
-    print(i+1, end=' ')
-    print(result[i]*1000)
+print(int(a))

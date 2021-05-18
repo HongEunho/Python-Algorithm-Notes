eb0f8fde18972b77eb0db16489ece66e2f304d3d
HongEunho <ghdwns315@naver.com>
2021-05-18 18:01:11 +0900

    Create Floyd-Warshall.py

---
+++ Floyd-Warshall/Floyd-Warshall.py
@@ -0,0 +1,29 @@
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=' ')
+        else:
+            print(graph[a][b], end=' ')
+
+    print()

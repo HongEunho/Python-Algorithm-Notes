3e7208068f29de034aed5e0a17eeee481b05eab5
HongEunho <ghdwns315@naver.com>
2021-05-30 22:21:46 +0900

    Update test2.py

--- DFS/test2.py
+++ DFS/test2.py
@@ -1,3 +1,15 @@
-a = "*"
+from itertools import combinations
 
-print(int(a))
+
+def solution(number, k):
+    answer = ''
+    length = len(number)
+
+    a = list(combinations(number, length - 2))
+    b = []
+    for i in range(len(a)):
+        b.append("".join(a[i]))
+
+    return max(b)
+
+print(solution("1231234", 3))

6320392d4a2535cc1ddbc02220923fcfb06e069e
HongEunho <ghdwns315@naver.com>
2021-06-01 22:49:36 +0900

    Create BaekJoon11279.py

---
+++ PriorityQueue/BaekJoon11279.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+n = int(input())
+q = []
+for i in range(n):
+    a = int(sys.stdin.readline().rstrip())
+    if a > 0:
+        heapq.heappush(q, -a)
+    else:
+        if not q:
+            print(0)
+        else:
+            print(-heapq.heappop(q))

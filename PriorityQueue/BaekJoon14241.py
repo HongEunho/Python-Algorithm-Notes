53e7aaeb50a11331c38bfb9e9f05347983c59bd9
HongEunho <ghdwns315@naver.com>
2021-06-14 02:03:37 +0900

    Update BaekJoon14241.py

--- PriorityQueue/BaekJoon14241.py
+++ PriorityQueue/BaekJoon14241.py
@@ -1,18 +1,17 @@
 import heapq
 n = int(input())
 slime = list(map(int, input().split()))
-queue = []
 
-for i in range(n):
-    heapq.heappush(queue, -slime[i])
+slime.sort()
 
 score = 0
-while len(queue) > 1:
-    a = -heapq.heappop(queue)
-    b = -heapq.heappop(queue)
-    print(a, end=' ')
-    print(b, end=' ')
+while len(slime) > 1:
+    a = slime.pop()
+    b = slime.pop()
+
     score += a*b
-    heapq.heappush(queue, a+b)
+    heapq.heappush(slime, a+b)
+
+
 
 print(score)

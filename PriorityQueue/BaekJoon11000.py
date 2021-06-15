f53daf394cbc63c2031e45909772641f1107b676
HongEunho <ghdwns315@naver.com>
2021-06-16 02:26:57 +0900

    Update BaekJoon11000.py

--- PriorityQueue/BaekJoon11000.py
+++ PriorityQueue/BaekJoon11000.py
@@ -1,4 +1,23 @@
+import heapq
 n = int(input())
 
+q = []
+
 for i in range(n):
-    
+    start, end = map(int, input().split())
+    q.append([start, end])
+
+q.sort()
+
+room = []
+heapq.heappush(room, q[0][1])
+
+for i in range(1, n):
+    if q[i][0] < room[0]: # 현재 회의실 끝나는 시간보다 다음 회의 시작시간이 빠르면
+        heapq.heappush(room, q[i][1]) # 새로운 회의실 개설
+    else: # 현재 회의실에 이어서 회의 개최 가능
+        heapq.heappop(room) # 새로운 회의로 시간 변경을 위해 pop후 새 시간 push
+        heapq.heappush(room, q[i][1])
+
+print(len(room))
+

da9eab8905f2c214a8eb2b6bb44fec389209ca38
HongEunho <ghdwns315@naver.com>
2021-09-06 22:51:24 +0900

    Update test.py

--- Greedy/test.py
+++ Greedy/test.py
@@ -1,21 +1,9 @@
-exp = input()
-ans = ''
+n, m = map(int, input().split())
 
-flag = 0 # 괄호 열기전
-for i in range(len(exp)):
-    if exp[i] == '-':
-        if flag == 0:
-            ans += exp[i]
-            ans += "("
-            flag = 1
-        else:
-            ans += ")"
-            ans += exp[i]
-            ans += "("
-    else:
-        ans += exp[i]
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
 
-if flag == 1:
-    ans += ")"
-print(ans)
-print(eval(ans))
+    result = max(min_value, result)
+

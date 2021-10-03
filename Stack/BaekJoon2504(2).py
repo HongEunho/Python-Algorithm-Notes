e07298cb892f63f29615359c4836c5ef34518027
HongEunho <ghdwns315@naver.com>
2021-10-03 12:51:49 +0900

    Create BaekJoon2504(2).py

---
+++ Stack/BaekJoon2504(2).py
@@ -0,0 +1,42 @@
+bracket = list(input())
+
+stack = []
+answer = 0
+
+for b in bracket:
+    if b == ')':
+        tmp = 0
+        while stack:
+            top = stack.pop()
+            if top == '(':
+                if tmp == 0:
+                    stack.append(2)
+                else:
+                    stack.append(2*tmp)
+                break
+            elif top == '[':
+                print(0)
+                exit(0)
+            else:
+                tmp += int(top)
+
+    elif b == ']':
+        tmp = 0
+        while stack:
+            top = stack.pop()
+            if top == '[':
+                if tmp == 0:
+                    stack.append(3)
+                else:
+                    stack.append(3 * tmp)
+                break
+            elif top == '(':
+                print(0)
+                exit(0)
+            else:
+                tmp += int(top)
+
+    else:
+        stack.append(b)
+
+print(stack)

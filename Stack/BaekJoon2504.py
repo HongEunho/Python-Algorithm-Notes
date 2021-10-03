ada8b1b7e2a0453f5324087629aff86d1c14149d
HongEunho <ghdwns315@naver.com>
2021-10-03 12:15:02 +0900

    Create BaekJoon2504.py

---
+++ Stack/BaekJoon2504.py
@@ -0,0 +1,39 @@
+bracket = list(input())
+
+stack = []
+answer = 0
+tmp = 1
+
+for i in range(len(bracket)):
+
+    if bracket[i] == "(":
+        stack.append(bracket[i])
+        tmp *= 2
+
+    elif bracket[i] == "[":
+        stack.append(bracket[i])
+        tmp *= 3
+
+    elif bracket[i] == ")":
+        if not stack or stack[-1] == "[":
+            answer = 0
+            break
+        if bracket[i-1] == "(":
+            answer += tmp
+        stack.pop()
+        tmp //= 2
+
+    else:
+        if not stack or stack[-1] == "(":
+            answer = 0
+            break
+        if bracket[i-1] == "[":
+            answer += tmp
+
+        stack.pop()
+        tmp //= 3
+
+if stack:
+    print(0)
+else:
+    print(answer)

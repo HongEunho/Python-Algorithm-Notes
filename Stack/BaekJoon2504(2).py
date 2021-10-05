b9bedab0c4b99ad0bb96a8ffd6e6d806be72e80b
HongEunho <ghdwns315@naver.com>
2021-10-05 21:53:51 +0900

    Update BaekJoon2504(2).py

--- Stack/BaekJoon2504(2).py
+++ Stack/BaekJoon2504(2).py
@@ -1,3 +1,4 @@
+import sys
 bracket = list(input())
 
 stack = []
@@ -6,35 +7,43 @@ answer = 0
 for b in bracket:
     if b == ')':
         tmp = 0
-        while stack:
-            top = stack.pop()
-            if top == '(':
-                if tmp == 0:
-                    stack.append(2)
+        if not stack:
+            print(0)
+            sys.exit(0)
+        else:
+            while stack:
+                top = stack.pop()
+                if top == '(':
+                    if tmp == 0:
+                        stack.append(2)
+                    else:
+                        stack.append(2*tmp)
+                    break
+                elif top == '[':
+                    print(0)
+                    sys.exit(0)
                 else:
-                    stack.append(2*tmp)
-                break
-            elif top == '[':
-                print(0)
-                exit(0)
-            else:
-                tmp += int(top)
+                    tmp += int(top)
 
     elif b == ']':
         tmp = 0
-        while stack:
-            top = stack.pop()
-            if top == '[':
-                if tmp == 0:
-                    stack.append(3)
+        if not stack:
+            print(0)
+            sys.exit(0)
+        else:
+            while stack:
+                top = stack.pop()
+                if top == '[':
+                    if tmp == 0:
+                        stack.append(3)
+                    else:
+                        stack.append(3 * tmp)
+                    break
+                elif top == '(':
+                    print(0)
+                    sys.exit(0)
                 else:
-                    stack.append(3 * tmp)
-                break
-            elif top == '(':
-                print(0)
-                exit(0)
-            else:
-                tmp += int(top)
+                    tmp += int(top)
 
     else:
         stack.append(b)

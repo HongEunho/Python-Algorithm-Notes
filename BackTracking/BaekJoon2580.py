c5250e4f7a193a354093bff4407aae017831fff1
HongEunho <ghdwns315@naver.com>
2021-10-08 00:23:51 +0900

    Update BaekJoon2580.py

--- BackTracking/BaekJoon2580.py
+++ BackTracking/BaekJoon2580.py
@@ -4,7 +4,16 @@ graph = []
 for i in range(9):
     graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
+def checkCol(x, col):
+    for i in range(9):
+        if x == graph[i][col]:
+            return False
+    return True
+
+
 def dfs():
 
     for i in range(9):
+        
+
         

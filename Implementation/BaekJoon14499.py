9d8ae0df206abfd9feaddf8ef8f2797ae49c303e
HongEunho <ghdwns315@naver.com>
2021-10-15 19:54:38 +0900

    Create BaekJoon14499.py

---
+++ Implementation/BaekJoon14499.py
@@ -0,0 +1,43 @@
+n, m, x, y, k = map(int, input().split())
+
+board = []
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+dice = [0, 0, 0, 0, 0, 0]
+
+def turn(dir):
+    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if dir == 1: #동
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+
+    elif dir == 2: #서
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+
+    elif dir == 3: #북
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
+
+    else:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+
+for i in range(n):
+    board.append(list(map(int, input().split())))
+
+comm = list(map(int, input().split()))
+
+nx, ny = x, y
+for i in comm:
+    nx += dx[i-1]
+    ny += dy[i-1]
+
+    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        nx -= dx[i-1]
+        ny -= dy[i-1]
+        continue
+    turn(i)
+    if board[nx][ny] == 0:
+        board[nx][ny] = dice[-1]
+    else:
+        dice[-1] = board[nx][ny]
+        board[nx][ny] = 0
+
+    print(dice[0])
